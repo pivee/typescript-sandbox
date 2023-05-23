7ad649afa0139e98b44db7807603b3e0e6bca977
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-05-23 17:22:19 +0530

    Add types on API routes

--- apps/nestjs-rest-api/src/app.controller.ts
+++ apps/nestjs-rest-api/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Responses } from 'types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  ping(): Responses.PingResponse {
+    return {
+      name: 'nestjs-rest-api',
+      version: '0.0.1',
+      environment: process.env.NODE_ENV,
+    };
+  }
+
+  @Get()
+  getHello(): Responses.DataResponse<string> {
+    return { data: this.appService.getHello() };
   }
 }
