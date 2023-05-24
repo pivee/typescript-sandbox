3164012a4754ba01388d3a3fb2cc66ab4a241380
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-05-24 18:49:28 +0530

    Add `child_process` script to `main.ts`

--- apps/nestjs-rest-api/src/main.ts
+++ apps/nestjs-rest-api/src/main.ts
@@ -1,19 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
 import { Logger } from '@nestjs/common';
-import * as swaggerUi from 'swagger-ui-express';
+import { NestFactory } from '@nestjs/core';
+import * as cp from 'child_process';
 import { readFileSync } from 'fs';
 import { join } from 'path';
+import * as swaggerUi from 'swagger-ui-express';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const swaggerDocument = JSON.parse(
-    readFileSync(join(__dirname, 'swagger.json'), 'utf8'),
-  );
+  if (process.env.NODE_ENV !== 'production') {
+    cp.execSync('pnpm swagger');
 
-  app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    const swaggerDocument = JSON.parse(
+      readFileSync(join(__dirname, 'swagger.json'), 'utf8'),
+    );
 
+    app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  }
   await app.listen(process.env.PORT ?? 4000, () => {
     Logger.log(
       `Running on port ${process.env.PORT ?? 4000}`,
